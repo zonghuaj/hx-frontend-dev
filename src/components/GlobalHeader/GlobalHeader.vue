55bab01e958e0ca0c6456ced5da6f8dfde72dd92
tony <8163944@qq.com>
2020-05-22 15:35:48 +0800

    feat: add top header

--- src/components/GlobalHeader/GlobalHeader.vue
+++ src/components/GlobalHeader/GlobalHeader.vue
@@ -5,11 +5,14 @@
         v-if="visible"
         :class="[fixedHeader && 'ant-header-fixedHeader', sidebarOpened ? 'ant-header-side-opened' : 'ant-header-side-closed', ]"
         :style="{ padding: '0' }">
+
+        <!-- vertical layout by zonghua -->
         <div v-if="mode === 'sidemenu'" class="header">
           <a-icon v-if="device==='mobile'" class="trigger" :type="collapsed ? 'menu-fold' : 'menu-unfold'" @click="toggle"/>
           <a-icon v-else class="trigger" :type="collapsed ? 'menu-unfold' : 'menu-fold'" @click="toggle"/>
           <user-menu></user-menu>
         </div>
+        <!-- horizontal layout by zonghua -->
         <div v-else :class="['top-nav-header-index', theme]">
           <div class="header-index-wide">
             <div class="header-index-left">
@@ -20,6 +23,7 @@
             <user-menu class="header-index-right"></user-menu>
           </div>
         </div>
+
       </a-layout-header>
     </div>
   </transition>
--- src/components/tools/UserMenu.vue
+++ src/components/tools/UserMenu.vue
@@ -1,38 +1,57 @@
 <template>
   <div class="user-wrapper">
+    <div>
+      <a-menu  class="usermenu-display-inline" mode="horizontal">
+        <a-menu-item key="mail1"> <a-icon type="mail" />基础信息</a-menu-item>
+        <a-menu-item key="mail2"> <a-icon type="mail" />基础信息</a-menu-item>
+        <a-menu-item key="mail3"> <a-icon type="mail" />基础信息</a-menu-item>
+        <a-menu-item key="mail4"> <a-icon type="mail" />基础信息</a-menu-item>
+        <a-menu-item key="mail5"> <a-icon type="mail" />基础信息</a-menu-item>
+        <a-menu-item key="mail6"> <a-icon type="mail" />基础信息</a-menu-item>
+        <a-menu-item key="app"> <a-icon type="appstore" />应收管理</a-menu-item>
+      </a-menu>
+    </div>
     <div class="content-box">
-      <a href="https://pro.loacg.com/docs/getting-started" target="_blank">
-        <span class="action">
-          <a-icon type="question-circle-o"></a-icon>
-        </span>
-      </a>
-      <notice-icon class="action"/>
-      <a-dropdown>
+      <notice-icon class="action" />
+       <a-dropdown>
         <span class="action ant-dropdown-link user-dropdown-menu">
-          <a-avatar class="avatar" size="small" :src="avatar"/>
+          <a-avatar
+            class="avatar"
+            size="small"
+            :src="avatar"
+          />
           <span>{{ nickname }}</span>
         </span>
-        <a-menu slot="overlay" class="user-dropdown-menu-wrapper">
+        <a-menu
+          slot="overlay"
+          class="user-dropdown-menu-wrapper"
+        >
           <a-menu-item key="0">
             <router-link :to="{ name: 'center' }">
-              <a-icon type="user"/>
+              <a-icon type="user" />
               <span>个人中心</span>
             </router-link>
           </a-menu-item>
           <a-menu-item key="1">
             <router-link :to="{ name: 'settings' }">
-              <a-icon type="setting"/>
+              <a-icon type="setting" />
               <span>账户设置</span>
             </router-link>
           </a-menu-item>
-          <a-menu-item key="2" disabled>
-            <a-icon type="setting"/>
+          <a-menu-item
+            key="2"
+            disabled
+          >
+            <a-icon type="setting" />
             <span>测试</span>
           </a-menu-item>
-          <a-menu-divider/>
+          <a-menu-divider />
           <a-menu-item key="3">
-            <a href="javascript:;" @click="handleLogout">
-              <a-icon type="logout"/>
+            <a
+              href="javascript:;"
+              @click="handleLogout"
+            >
+              <a-icon type="logout" />
               <span>退出登录</span>
             </a>
           </a-menu-item>
@@ -47,36 +66,41 @@ import NoticeIcon from '@/components/NoticeIcon';
 import { mapActions, mapGetters } from 'vuex';
 
 export default {
-  name: 'UserMenu',
-  components: {
-    NoticeIcon
-  },
-  computed: {
-    ...mapGetters(['nickname', 'avatar'])
-
-  },
-  methods: {
-    ...mapActions(['Logout']),
-    handleLogout () {
-      this.$confirm({
-        title: '提示',
-        content: '真的要注销登录吗 ?',
-        onOk: () => {
-          return this.Logout({}).then(() => {
-            setTimeout(() => {
-              window.location.reload();
-            }, 16);
-          }).catch(err => {
-            this.$message.error({
-              title: '错误',
-              description: err.message
-            });
-          });
-        },
-        onCancel () {
-        }
-      });
-    }
-  }
+	name: 'UserMenu',
+	components: {
+		NoticeIcon
+	},
+	computed: {
+		...mapGetters(['nickname', 'avatar'])
+	},
+	methods: {
+		...mapActions(['Logout']),
+		handleLogout () {
+			this.$confirm({
+				title: '提示',
+				content: '真的要注销登录吗 ?',
+				onOk: () => {
+					return this.Logout({})
+						.then(() => {
+							setTimeout(() => {
+								window.location.reload();
+							}, 16);
+						})
+						.catch(err => {
+							this.$message.error({
+								title: '错误',
+								description: err.message
+							});
+						});
+				},
+				onCancel () {}
+			});
+		}
+	}
 };
 </script>
+<style scoped>
+  .usermenu-display-inline {
+    display: inline-block;
+  }
+</style>
